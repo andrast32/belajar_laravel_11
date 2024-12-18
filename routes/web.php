4ce511b0cd6dae04ad94fa60e327cc9c758fa4bb
andra343 <setiawanananda32@gmail.com>
2024-12-18 09:52:46 +0700

    view data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,48 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About Page'], ['nama' => '@andrast32']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'judul artikel 1',
+            'author' => 'Andrast',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum tempore deleniti impedit placeat repellat quos repellendus neque excepturi quidem officiis nostrum earum error aliquid est aperiam quasi sapiente, accusamus nam.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'judul artikel 2',
+            'author' => 'Andrast',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio dolor architecto labore officia distinctio unde voluptas in minus iusto nam corporis sunt consectetur, dolorum est perferendis? Libero reiciendis fugiat nam.'
+        ]
+    ]]);
+});
+
+route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'judul artikel 1',
+            'author' => 'Andrast',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum tempore deleniti impedit placeat repellat quos repellendus neque excepturi quidem officiis nostrum earum error aliquid est aperiam quasi sapiente, accusamus nam.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'judul artikel 2',
+            'author' => 'Andrast',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio dolor architecto labore officia distinctio unde voluptas in minus iusto nam corporis sunt consectetur, dolorum est perferendis? Libero reiciendis fugiat nam.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
